5c3a1e91f10e9ba93ec0e1ca5c64fc924844f54e
Denis Tauchman <u18.428@pslib.cloud>
2020-04-27 12:03:17 +0200

    test

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Adventure2020
+namespace RPG
 {
     public class Program
     {
