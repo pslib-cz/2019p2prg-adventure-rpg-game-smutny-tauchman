13e33253df0080dfde9ecf8e3209ec4603a17020
Jan Smutný <u18.426@pslib.cloud>
2020-05-17 16:49:05 +0200

    commit

--- RPG/wwwroot/js/site.js
+++ RPG/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+const init = function () {
+    let audio = document.getElementById("deadSilence");
+    audio.volume = 0.01;
+    let titles = document.querySelectorAll(".fadeIn p");
+    let time = 0;
+    titles.forEach(element => {
+        element.style.animationDelay = time + "s";
+        time += 1;
+    });
+}
+document.addEventListener("DOMContentLoaded", init);
